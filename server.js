74e537c59ea54831adf5d744377dbf0963aba4a4
yash-2507 <imyash.654@gmail.com>
2022-08-24 02:25:45 +0530

    Done

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
-const mongoose = require("mongoose");
-const { connectDB } = require("./config/dbConn");
-const cookieParser = require("cookie-parser");
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const mongoose = require('mongoose');
+const { connectDB } = require('./config/dbConn');
+const cookieParser = require('cookie-parser');
 
-const dotenv = require("dotenv");
-const { verifyJwt } = require("./middleware/auth");
+const dotenv = require('dotenv');
+const { verifyJwt } = require('./middleware/auth');
 dotenv.config();
 
 // Connect to database
@@ -21,35 +21,27 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // CORS
-const whitelist = ["http://localhost:3000"];
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1 || origin === undefined) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    optionSuccesStatus: 200,
-    credentials: true,
+    origin: true, //included origin as true
+    credentials: true, //included credentials as true
 };
 app.use(cors(corsOptions));
 
 // Set static folder
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Defining Port
 const PORT = process.env.PORT || 8080;
 
 // Routes
-app.use("/auth", require("./routes/auth"));
-app.get("/me", verifyJwt,  (req, res) => {
-    res.status(200).json({success: true, message: "Yeah! It's Protected..."})
-})
+app.use('/auth', require('./routes/auth'));
+app.get('/me', verifyJwt, (req, res) => {
+    res.status(200).json({ success: true, message: "Yeah! It's Protected..." });
+});
 
 // Connecting to MongoDB and starting Server
-mongoose.connection.once("open", () => {
-    console.log("Connected to MongoDB");
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
     app.listen(PORT, () => {
         try {
             console.log(`Server started at PORT ${PORT}`);
