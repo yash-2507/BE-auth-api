c25752f48a214ebd5dd4b40e6093e9ab5929d88b
yash-2507 <imyash.654@gmail.com>
2022-08-24 17:53:37 +0530

    done

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const login = require('../controllers/loginController');
-const register = require('../controllers/registerController');
-const router = require('express').Router();
-
-router.post("/register", register)
-router.post("/login", login)
-
-module.exports = router;
